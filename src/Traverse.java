d7cb6cee727bf49115fadab84a4a417750938748
jmakho01 <210610870+jmakho01@users.noreply.github.com>
2026-04-09 14:05:31 -0700

    Finished Livecode Day 1

--- src/Traverse.java
+++ src/Traverse.java
@@ -28,6 +28,40 @@ public class Traverse {
     v45.neighbors = new ArrayList<>(List.of(v23));
     v23.neighbors = new ArrayList<>(List.of());
     v67.neighbors = new ArrayList<>(List.of(v91));
+
+    //traverse(v7);
+    System.out.println(sum(v7));
+  }
+
+  public static int sum(Vertex<Integer> current) {
+    return sum(current, new HashSet<>());
+  }
+
+  // sum of all values of reachable verticies (including current)
+  private static int sum(Vertex<Integer> current, Set<Vertex<Integer>> visited) {
+    if (current == null || visited.contains(current)) return 0;
+    visited.add(current);
+    int total = current.data;
+    for(Vertex<Integer> neighbor : current.neighbors) {
+      total += sum(neighbor, visited);
+    }
+    return total;
+  }
+
+  public static void traverse(Vertex<?> current) {
+    Set<Vertex<?>> visited = new HashSet<>();
+    traverse(current, visited);
+  }
+
+  // print every vertex value reachable from current (including current itself)
+  private static void traverse(Vertex<?> current, Set<Vertex<?>> visited) {
+    if (current == null) return;
+    if (visited.contains(current)) return;
+    visited.add(current);
+    System.out.println(current.data);
+    for(Vertex<?> neighbor : current.neighbors) {
+      traverse(neighbor, visited);
+    }
   }
 
 }
